44bd3eb3cefed0afc1368e320a6bedfe694a01f3
SerpNaga <TyurinaAA20@st.ithub.ru>
2022-06-23 20:47:12 +0300

    Text bugs fixed!

--- src/components/App.js
+++ src/components/App.js
@@ -56,7 +56,8 @@ function App() {
       socket.emit('pong', { id: socket.id, message: 'pong' })
     }, 3000);
     socket&&socket.on("addednotif", ({id, text, user, date, type})=>{
-      console.log({date, nowDate:new Date()})
+      new Date(date)<=new Date?
+      enqueueSnackbar(text, {variant:type.toLowerCase()}):
       dispatch(addNotif({id, text, user, date, type}))
     })
     socket&&socket.on("delnotif", ({id,})=>{
